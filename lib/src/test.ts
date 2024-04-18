e425ae49aa4addf9922ddae48288918f059ff255
lab900-senn-lambrecht <senn.lambrecht@lab900.com>
2024-04-18 13:11:01 +0200

    chore: angular 17

--- lib/src/test.ts
+++ lib/src/test.ts
@@ -1,5 +1,5 @@
-import 'zone.js/dist/zone';
-import 'zone.js/dist/zone-testing';
+import 'zone.js';
+import 'zone.js/zone-testing';
 import { getTestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
