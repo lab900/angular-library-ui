1b408247bdd8a23016a63711cffcc0965e083890
lab900-senn-lambrecht <senn.lambrecht@lab900.com>
2024-07-17 10:20:48 +0200

    chore: ci/cd flow update

--- deploy-manual.bat
+++
@@ -1,7 +0,0 @@
-cd lib/
-call npm version patch
-cd ..
-call npm run build:ui:prod
-cd dist/@lab900/ui
-call npm publish --ignore-scripts
-
--- deploy-manual.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Only used for manual deploys
-cd lib/ || exit
-npm version prerelease --preid alpha
-cd ..
-npm run build:ui:prod
-cd dist/@lab900/ui || exit
-npm publish --ignore-scripts
